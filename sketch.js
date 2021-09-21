2c30916be8f461a21097ce64d83a92b8dbc86055
vishwapandya <57280185+vishwapandya@users.noreply.github.com>
2021-09-21 14:28:41 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,10 @@
+
+
 var ball = {
-  x:20,
-  y:30,
-  r:30,
-  xspeed:0,
-  yspeed:0, 
-  color:["blue","red","green","purple"],
- 
+
+  
+  
+  
 };
 
 function setup() 
@@ -16,11 +15,10 @@ function setup()
 
 function draw() 
 {
-  background(220);
-  circle(ball.x,ball.y,ball.r);
-  fill(ball.color[0]);
-  ball.xspeed = 1;
-  ball.x= ball.x + ball.xspeed;
+  background("white");
+
+  
+  
     
 }
 
